74ff85114886fa03fe3489b2b58c41ef4554de22
hujingyue <591711447@qq.com>
2019-04-30 21:37:35 +0800

    Codelgniter

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -2,8 +2,24 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User extends CI_Controller {
-	public function index()
+	public function login()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('login');
 	}
+	public function check_login(){
+        $uname = $this->input->post('username');
+        $pwd = $this->input->post('password');
+
+        $this->load->model('user_model');
+
+        $user = $this->user_model->get_by_user_and_pwd($uname,$pwd);
+
+        if ($user){
+//            echo '登录成功';
+            $this->load->view('success');
+        }else{
+            echo "登录失败";
+        }
+    }
 }
+//http://localhost/myblog/index.php/user/login
